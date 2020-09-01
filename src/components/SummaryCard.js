5f321e26c2a8b200496692a02b1cae279a37dfcd
Fabian Flores <fabian.flores96@gmail.com>
2020-09-01 16:01:47 -0700

    Styling changes needed in IconsList

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Linkedin, Github, Twitter } from 'grommet-icons';
 import styled from 'styled-components';
 
+const Container = styled.div(({ noPadding, column, justifyContent, alignItems }) => `
+    padding: ${noPadding ? '' : '1rem'};
+    display: flex;
+    flex-direction: ${column ? 'column' : 'row'};
+    justify-content: ${justifyContent || ''};
+    align-items: ${alignItems || ''};
+`);
+
 const Card = styled.div`
+    display: flex;
+    flex-wrap: wrap;
     background-color: whitesmoke;
     border-radius: 10px;
     box-shadow: 0 5px 20px grey;
     margin: 0 auto;
     overflow: hidden;
-    width: 100%;
+
+    @media only screen and (min-width: 24rem) {
+        flex-direction: column;
+    }
 `;
 
 const Header = styled.div`
@@ -15,35 +29,45 @@ const Header = styled.div`
     font-size: 0.8rem;
     justify-content: space-between;
     padding: 0 1rem;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid grey;
+    width: 100%;
 
-    @media only screen and (min-width: 48rem) {
+    @media only screen and (min-width: 24rem) {
         font-size: 1rem;
+        width: inherit;
     }
 `;
 
-const Container = styled.div(({ noPadding, column, justifyContent }) => `
-    padding: ${noPadding ? '' : '1rem'};
+const Body = styled.div`
     display: flex;
-    flex-direction: ${column ? 'column' : 'row'};
-    justify-content: ${justifyContent || ''};
-`);
+    flex-direction: column;
+    padding: 1rem;
+
+    @media only screen and (min-width: 38rem) {
+        flex-direction: row;
+    }
+`;
 
 const Portrait = styled.div`
     height: 20rem;
-    width: 14rem;
+    width: 100%;
     border-radius: 10px;
     align-self: center;
     background-color: skyblue;
 
-    @media only screen and (min-width: 48rem) {
+    @media only screen and (min-width: 46rem) {
         height: 22rem;
         width: 16rem;
+    }
+    
+    @media only screen and (min-width: 38rem) {
+        height: 16rem;
+        width: 14rem;
     }
 `;
 
 const FieldName = styled.span`
-    font-size: 1rem;
+    font-size: 0.8rem;
     margin-right: 1rem;
 `;
 
@@ -52,44 +76,53 @@ const FieldContent = styled.span`
     font-weight: bold;
 `;
 
+const IconsList = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 10rem;
+
+    @media only screen and (min-width: 24rem) {
+        max-width: 16rem;
+    }
+`;
+
+const Spacer = styled.div`
+    flex: 1;
+`;
+
 const SummaryCard = () => {
-    const nameSection = (
-        <Container noPadding column>
-            <Container noPadding>
-                <FieldName>FN</FieldName>
-                <FieldContent>Fabian Dean</FieldContent>
-            </Container>
-            <Container noPadding>
-                <FieldName>LN</FieldName>
-                <FieldContent>Flores</FieldContent>
-            </Container>
+    const fieldNameSection = (
+        <Container alignItems='flex-end' noPadding column>
+            <FieldName>FN</FieldName>
+            <FieldName>LN</FieldName>
+            <Spacer />
+            <FieldName>Focus</FieldName>
+            <Spacer />
+            <FieldName>Degree</FieldName>
+            <FieldName>Major</FieldName>
+            <FieldName>School</FieldName>
         </Container>
     );
 
-    const focusSection = (
+    const fieldContentSection = (
         <Container noPadding column>
-            <Container noPadding>
-                <FieldName>Focus</FieldName>
-                <FieldContent>{'Web & Mobile App Development'}</FieldContent>
-            </Container>
+            <FieldContent>Fabian Dean</FieldContent>
+            <FieldContent>Flores</FieldContent>
+            <Spacer />
+            <FieldContent>Web & Mobile Apps</FieldContent>
+            <Spacer />
+            <FieldContent>Bachelor of Science</FieldContent>
+            <FieldContent>Computer Science</FieldContent>
+            <FieldContent>Cal Poly Pomona</FieldContent>
         </Container>
     );
 
-    const educationSection = (
-        <Container noPadding column>
-            <Container noPadding>
-                <FieldName>Degree</FieldName>
-                <FieldContent>Bachelor of Science</FieldContent>
-            </Container>
-            <Container noPadding>
-                <FieldName>Major</FieldName>
-                <FieldContent>Computer Science</FieldContent>
-            </Container>
-            <Container noPadding>
-                <FieldName>School</FieldName>
-                <FieldContent>California Polytechnic State University, Pomona</FieldContent>
-            </Container>
-        </Container>
+    const socialsSection = (
+        <IconsList>
+            <Linkedin />
+            <Github />
+            <Twitter />
+        </IconsList>
     );
 
     return (
@@ -98,15 +131,21 @@ const SummaryCard = () => {
                 <h1>Software Engineer</h1>
                 <h1>{'</>'}</h1>
             </Header>
-            <Container>
-                <Portrait />
-                <Container column justifyContent='space-between'>
-                    {nameSection}
-                    {focusSection}
-                    {educationSection}
+            <Body>
+                <Container noPadding column>
+                    <Portrait />
                 </Container>
-            </Container>
-        </Card>
+                <Container justifyContent='space-around'>
+                    {fieldNameSection}
+                    {fieldContentSection}
+                    <Spacer />
+                </Container>
+                <Spacer />
+                <Container noPadding justifyContent='center'>
+                    {socialsSection}
+                </Container>
+            </Body>
+        </Card >
     );
 };
 
