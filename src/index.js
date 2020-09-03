c2b4f87816fab0aaaf48c27c0d5f1e026e600d0e
Fabian Flores <fabian.flores96@gmail.com>
2020-09-03 00:58:22 -0700

    Changed font-family to Lekton from Google Fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './fonts/Lekton-Regular.ttf';
+import './fonts/Lekton-Bold.ttf';
+import './fonts/Lekton-Italic.ttf';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
